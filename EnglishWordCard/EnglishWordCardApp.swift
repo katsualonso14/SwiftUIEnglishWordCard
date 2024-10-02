7804083a8d4c426106880c0b6ba9cb998127c75c
katsuyatamai <xabi.k0124football@gmail.com>
2024-10-02 11:39:39 +0200

    project start

---
+++ EnglishWordCard/EnglishWordCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishWordCardApp.swift
+//  EnglishWordCard
+//
+//  Created by KatsuyaTamai on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct EnglishWordCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishWordCardTests/EnglishWordCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishWordCardTests.swift
+//  EnglishWordCardTests
+//
+//  Created by KatsuyaTamai on 2024/10/02.
+//
+
+import Testing
+@testable import EnglishWordCard
+
+struct EnglishWordCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
