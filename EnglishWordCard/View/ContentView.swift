a711ebb418d73d6f2f89ff912b79c3a1b0654e9d
katsuyatamai <xabi.k0124football@gmail.com>
2024-10-30 07:59:10 +0100

    fix navi bar

--- EnglishWordCard/View/ContentView.swift
+++ EnglishWordCard/View/ContentView.swift
@@ -10,10 +10,11 @@ struct ContentView: View {
     @State private var cardItemEdit: CardItem?
     @Query private var items: [CardItem]
     
-//    init() {
-//        setNavigationBarColor()
-//    }
-
+    init() {
+        setNavigationBarColor()
+    }
+    
+    
     var body: some View {
         NavigationStack {
             List {
@@ -23,28 +24,28 @@ struct ContentView: View {
                         VStack(alignment: .leading) {
                             
                             Text(item.title)
-                                .font(.largeTitle)
+                                .font(.title)
                                 .bold()
                             
                             Text(item.contents)
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                         }
-
-                            Spacer()
-                            Button {
-                                withAnimation{
-                                    item.isCompleted.toggle()
-                                    try? context.save()
+                        
+                        Spacer()
+                        Button {
+                            withAnimation{
+                                item.isCompleted.toggle()
+                                try? context.save()
                                 
-                                }
-                            } label: {
-                                Image(systemName: "checkmark")
-                                    .symbolVariant(.circle.fill)
-                                    .foregroundStyle(item.isCompleted ? .green : .gray)
-                                    .font(.largeTitle)
-                            }.buttonStyle(.plain)
-                        }
+                            }
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? .green : .gray)
+                                .font(.largeTitle)
+                        }.buttonStyle(.plain)
+                    }
                     
                     .contentShape(Rectangle()) // 外側に適用
                     .onTapGesture {
@@ -56,9 +57,8 @@ struct ContentView: View {
                             // itemの削除
                             context.delete(item)
                             try? context.save() // 削除後にデータを保存
-                      
                         }
-                               label: {
+                        label: {
                             Label("Delete", systemImage: "trash")
                                 .symbolVariant(.fill)
                         }
@@ -67,68 +67,60 @@ struct ContentView: View {
                             cardItemEdit = item
                             try? context.save() // 更新後にデータを保存
                         }
-                               label: {
+                        label: {
                             Label("Edit", systemImage: "pencil")
-                               }.tint(.orange)
+                        }.tint(.orange)
                         
                     }
                 }
             }
-            .navigationTitle("WordCard")
-                .toolbar {
-                    ToolbarItem {
-                        Button(
-                            action: {
-                                showCreate.toggle()
-                            },
-                            label:  {
-                                Label("Add Item", systemImage: "plus")
-                            })
-                    }
+            .navigationTitle("EnglishWordCard")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Button(
+                        action: {
+                            showCreate.toggle()
+                        },
+                        label:  {
+                            Label("Add Item", systemImage: "plus")
+                        })
+                    .tint(.white)
+
                 }
+            }
             // 下からのシート表示
-                .sheet(isPresented: $showCreate,
-                       content: {
-                    NavigationStack {
-                        CreateCardView()
-                    }
-                    .presentationDetents([.medium]) // 小さめで
-                })
-                .sheet(item: $cardItemEdit) {
-                    cardItemEdit = nil
-                } content: {
-                    item in
-                    UpdateCardView(item: item)
+            .sheet(isPresented: $showCreate,
+                   content: {
+                NavigationStack {
+                    CreateCardView()
                 }
-            
+                .presentationDetents([.medium]) // 小さめで
+            })
+            .sheet(item: $cardItemEdit) {
+                cardItemEdit = nil
+            } content: {
+                item in
+                UpdateCardView(item: item)
+            }
         }
-        
-        
-//        NavigationStack {
-//            VStack {
-//                Text("Hello!")
-//            }
-//            .navigationBarTitle("EnglishWordCard", displayMode: .inline)
-//            .accessibilityAddTraits(.isHeader)
-//    
-//            
-                    }
     }
+}
 
     
     
     
-    // ナビゲーションバーのColor設定
-//    private func setNavigationBarColor() {
-//        let appearance = UINavigationBarAppearance()
-//        appearance.configureWithOpaqueBackground()
-//        appearance.backgroundColor = UIColor.systemRed  // BackGroud Calor
-//        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]// textColor
-//        appearance.titleTextAttributes = [.foregroundColor: UIColor.white,
-//                                          .font : UIFont.systemFont(ofSize: 20, weight: .bold)]
-//        UINavigationBar.appearance().standardAppearance = appearance
-//        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-//    }
+//     ナビゲーションバーのColor設定
+private func setNavigationBarColor() {
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundColor = UIColor.systemRed  // BackGroud Calor
+    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]// textColor
+    appearance.titleTextAttributes = [.foregroundColor: UIColor.white,
+                                      .font : UIFont.systemFont(ofSize: 20, weight: .bold)]
+    UINavigationBar.appearance().standardAppearance = appearance
+    UINavigationBar.appearance().scrollEdgeAppearance = appearance
+}
 
 
 #Preview {
